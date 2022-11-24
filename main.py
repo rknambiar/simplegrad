cfe8a309ca13448fbd3531642a8b5a41a11e0c79
rohit517 <rohithkrishna517@gmail.com>
2022-11-23 21:27:03 -0800

    Display op in graph nodes

--- main.py
+++ main.py
@@ -14,29 +14,58 @@ def draw(tensor):
             if parent not in visited:
                 to_visit.append(parent)
             
-            edges.append((parent.name, curr_tensor.name))
+            edges.append((parent.name, curr_tensor.name, parent.grad))
             conn_str = f"From {curr_tensor.name} --> {parent.name}"
             print(conn_str)
         
         visited.add(curr_tensor)
         
-    f = graphviz.Digraph('network', filename="network.gv")
+    f = graphviz.Digraph('network', filename="network.gv",
+                         node_attr={'style': 'filled',
+                                    'color': 'lightblue2',
+                                    'shape': 'box'}
+    )
+
     for node in visited:
-        f.node(node.name, node.name)
+        label = node.name if node.op == "" else node.name + " | " + node.op
+        f.node(node.name, label)
 
     for edge in edges:
-        f.edge(edge[0], edge[1])
+        f.edge(edge[0], edge[1], fontcolor='red')
 
     f.view()
 
+def backward(tensor: Tensor):
+    """Computes gradient from this tensor backwards"""
+    def _topsort(t):
+        visited = set()
+        output = []
+        def _run_topsort(t):
+            if t not in visited:
+                visited.add(t)
+                for parent in t.parents:
+                    _run_topsort(parent)
+                output.append(t)
+        
+        _run_topsort(t)
+        return output
+    
+    topsort = _topsort(tensor)
+
+    tensor.grad = 1
+    for t in reversed(topsort):
+        t.backward_fun()
+
+
 def main():
     t1 = Tensor(3.4)
-    t2 = Tensor(2)
+    t2 = Tensor(2.6)
+    t3 = Tensor(5)
 
-    t3 = t1 + t2
-    t4 = t1 * t3
-    t5 = t4 * t2
+    t4 = t1 + t2
+    t5 = t3 * t4
 
+    backward(t5)
     draw(t5)
 
 if __name__ == "__main__":
--- tensor.py
+++ tensor.py
@@ -5,10 +5,15 @@ class Tensor:
     counter = 1
 
     def __init__(self, 
-                value: float) -> None:
+                value: float,
+                op = '') -> None:
         self.value = value
         self.parents = []
-        self.name = "Tensor" + str(Tensor.counter)
+        self.name = "Tensor_" + str(Tensor.counter)
+
+        self.grad = 0.
+        self.backward_fun = lambda: None
+        self.op = op # For displaying which op created the tensor
 
         Tensor.counter += 1
 
@@ -19,13 +24,29 @@ class Tensor:
         return f"Tensor: {self.name} value: {self.value}"
 
     def __add__(self, o) -> Tensor:
-        output = Tensor(self.value + o.value)
+        output = Tensor(self.value + o.value, op="add")
         output.parents.extend([self, o])
+        
+        def backward():
+            print(f"add backward node: {output.name}")
+            self.grad += output.grad
+            o.grad += output.grad
+
+        output.backward_fun = backward
+
         return output
 
     def __mul__(self, o) -> Tensor:
-        output = Tensor(self.value * o.value)
+        output = Tensor(self.value * o.value, op="mul")
         output.parents.extend([self, o])
+        
+        def backward():
+            print(f"mul backward node: {output.name}")
+            self.grad += output.grad * o.value
+            o.grad += output.grad * self.value
+
+        output.backward_fun = backward
+        
         return output
 
     
